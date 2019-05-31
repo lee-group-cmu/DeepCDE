87938dbb7ffc884f29bf9957c638469cb48e94d2
Nic Dalmasso <ndalmass@andrew.cmu.edu>
2019-05-31 16:30:04 -0400

    Adding tests on the pytorch loss function and slight change in notation

--- deepcde/deepcde_pytorch.py
+++ deepcde/deepcde_pytorch.py
@@ -24,8 +24,8 @@ class cde_loss(nn.Module):
 
     def forward(self, beta, z_basis, shrink_factor):
         complexity_terms = (beta**2).sum(dim=1) + 1.0
-        first_terms = (beta * z_basis.view(-1, 1).expand_as(beta)).sum(dim=1) + 1.0
-        loss = ((complexity_terms - 2 * first_terms) / shrink_factor).float().mean()
+        fit_terms = (beta * z_basis.view(-1, 1).expand_as(beta)).sum(dim=1) + 1.0
+        loss = (complexity_terms - 2 * fit_terms).float().mean() / shrink_factor
         return loss
 
 class cde_nll_loss(nn.Module):
@@ -34,8 +34,8 @@ class cde_nll_loss(nn.Module):
         super(cde_nll_loss, self).__init__()
 
     def forward(self, beta, z_basis, shrink_factor):
-        first_terms = (beta * z_basis.view(-1, 1).expand_as(beta)).sum(dim=1) + 1.0
-        loss = ((- 1 * first_terms) / shrink_factor).float().mean()
+        fit_terms = (beta * z_basis.view(-1, 1).expand_as(beta)).sum(dim=1) + 1.0
+        loss = ((- 1 * fit_terms) / shrink_factor).float().mean()
         return loss
 
 
--- deepcde/tests.py
+++ deepcde/tests.py
@@ -1,7 +1,9 @@
 import numpy as np
 import unittest2 as unittest
+import torch
 
 from utils import box_transform, normalize
+from deepcde_pytorch import cde_loss
 
 
 class MyTest(unittest.TestCase):
@@ -29,6 +31,23 @@ class MyTest(unittest.TestCase):
 
         self.assertTrue((1- epsilon) < np.trapz(example_density, example_range) < (1 + epsilon))
         self.assertTrue(np.sum(example_density[idx_negative]) == 0)
+
+
+    def test__loss_pytorch(self):
+        beta = np.repeat(a=np.arange(0, 10, 1).reshape(-1, 1), axis=1, repeats=10)
+        z_basis = np.arange(0, 10, 1).reshape(-1, 1)
+
+        ## Numpy computation
+        fit_terms = (beta * z_basis).sum(axis=1) + 1
+        comp_terms = (beta**2).sum(axis=1) + 1
+        loss_val = np.average((comp_terms - 2*fit_terms))
+
+        ## Pytorch Computation
+        beta_t = torch.from_numpy(beta)
+        z_basis_t = torch.from_numpy(z_basis)
+        criterion = cde_loss()
+
+        self.assertEqual(loss_val, criterion(beta_t, z_basis_t, 1).item())
 
 
 if __name__ == '__main__':
