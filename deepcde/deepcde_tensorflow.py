554288b3ea652b6e07500164b5e1ee7ea7c7313f
Nic Dalmasso <ndalmass@andrew.cmu.edu>
2019-06-17 18:07:54 -0400

    Adding changes in the cde_predict function. We need to do a basis evaluation before being able to do the prediction, which was not done before. Also, adding a default for shrink_factor and marginal_beta to get the example in the CDE tools paper a bit lighter

--- deepcde/deepcde_tensorflow.py
+++ deepcde/deepcde_tensorflow.py
@@ -3,20 +3,20 @@ from utils import normalize
 
 ### LOSS FUNCTIONS
 
-def cde_loss(beta, z_basis, shrink_factor, name="cde_loss"):
+def cde_loss(beta, z_basis, shrink_factor=1.0, name="cde_loss"):
     with tf.name_scope(name):
         complexity_terms = tf.reduce_sum(tf.square(beta), axis=1) + 1.0
         fit_terms = tf.reduce_sum(tf.multiply(beta, z_basis), axis=1) + 1.0
         loss = tf.reduce_mean(complexity_terms - 2 * fit_terms) / shrink_factor
         return loss
 
-def nll_loss(beta, z_basis, shrink_factor, name="nll_loss"):
+def nll_loss(beta, z_basis, shrink_factor=1.0, name="nll_loss"):
     with tf.name_scope(name):
         fit_terms = tf.reduce_sum(tf.multiply(beta, z_basis), axis=1) + 1.0
         loss = tf.reduce_mean(-fit_terms) / shrink_factor
         return loss
 
-def approx_cde_loss(beta, z_basis, shrink_factor, name="approx cde_loss"):
+def approx_cde_loss(beta, shrink_factor=1.0, name="approx cde_loss"):
     with tf.name_scope(name):
         complexity_terms = tf.reduce_sum(tf.square(beta), axis=1) + 1.0
         loss = tf.reduce_mean(-1 * complexity_terms) / shrink_factor
@@ -25,11 +25,11 @@ def approx_cde_loss(beta, z_basis, shrink_factor, name="approx cde_loss"):
 
 ### EXTRA LAYER (before softmax)
 
-def cde_layer(inputs, weight_sd, marginal_beta, name="cde_layer"):
+def cde_layer(inputs, weight_sd, marginal_beta=None, name="cde_layer"):
     with tf.name_scope(name):
         W_shape = [int(inputs.shape[1]), len(marginal_beta) - 1]
         W = tf.Variable(tf.random_normal(W_shape, stddev=weight_sd), name="W")
-        b = tf.get_variable("b", initializer=marginal_beta[1:])
+        b = tf.get_variable("b", initializer=marginal_beta[1:]) if marginal_beta else tf.get_variable("b")
         beta = tf.matmul(inputs, W) + b
         # tf.summary.histogram("weights", W)
         # tf.summary.histogram("biases", b)
@@ -39,13 +39,14 @@ def cde_layer(inputs, weight_sd, marginal_beta, name="cde_layer"):
 
 ### PREDICTION FUNCTION
 
-def cde_predict(sess, beta, z_min, z_max, z_grid, z_grid_basis, input_dict,
+def cde_predict(sess, beta, z_min, z_max, z_grid, basis, n_basis, input_dict,
                 delta=None, bin_size=0.01):
     beta = sess.run(beta, feed_dict=input_dict)
     n_obs = beta.shape[0]
     beta = np.hstack((np.ones((n_obs, 1)), beta))
+    z_grid_basis = basis.evaluate(z_grid)[:, n_basis]
     cdes = np.matmul(beta, z_grid_basis.T)
-    if delta is not None:
+    if delta:
         remove_bumps(cdes, delta=delta, bin_size=bin_size)
     normalize(cdes)
     cdes /= np.prod(z_max - z_min)
